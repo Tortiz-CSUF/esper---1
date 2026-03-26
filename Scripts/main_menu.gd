637438f68b7e9b486e2f72e6dc3a8d6b8bfffaf0
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-26 23:19:09 +0000

    Haunted_Adventure_2D_6

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,5 +1,11 @@
 extends Control
 
+@onready var begin_button: Button  = $MenuContainer/LeftPanel/BeginButton
+@onready var settings_button: Button = $MenuContainer/LeftPanel/SettingsButton
+@onready var quit_button: Button = $MenuContainer/LeftPanel/QuitButton
+@onready var selection_indicator: ColorRect = $MenuContainer/SelectionIndicator
+@onready var click_sfx: AudioStreamPlayer = $MenuContainer/ClickSFX
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
