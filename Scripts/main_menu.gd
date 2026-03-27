b4317a3c356193c9b92b84cc289a6d2e4900f35a
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-27 00:46:26 +0000

    Haunted_Adventure_2D_6

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,10 +1,24 @@
 extends Control
 
+# MAin Menu Hooks
 @onready var begin_button: Button  = $MenuContainer/LeftPanel/BeginButton
 @onready var settings_button: Button = $MenuContainer/LeftPanel/SettingsButton
 @onready var quit_button: Button = $MenuContainer/LeftPanel/QuitButton
 @onready var selection_indicator: ColorRect = $SelectionIndicator
 @onready var click_sfx: AudioStreamPlayer = $ClickSFX
+
+# Map Selection Hooks
+@onready var map_select_panel: VBoxContainer = $MapSelectPanel
+@onready var small_button: Button = $MapSelectPanel/SmallButton
+@onready var medium_button: Button = $MapSelectPanel/MediumButton
+@onready var large_button: Button = $MapSelectPanel/LargeButton
+@onready var map_back_button: Button = $MapSelectPanel/MapBackButton
+
+# Settings Hooks
+@onready var settings_panel: VBoxContainer = $SettingsPanel
+@onready var music_slider: HSlider = $SettingsPanel/MusicSlider
+@onready var sfx_slider: HSlider = $SettingsPanel/SFXSlider
+@onready var settings_back_button: Button = $SettingsPanel/SettingsBackButton
 
 var menu_buttons: Array[Button] = []
 
