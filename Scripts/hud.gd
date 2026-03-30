4275dd1377a199e8524719bb4b56887c333190b8
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-30 03:34:46 +0000

    Haunted_Adventure_2D_18

---
+++ Scripts/hud.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+@onready var heart1: TextureRect = $HealthContainer/Heart1
+@onready var heart2: TextureRect = $HealthContainer/Heart2
+@onready var heart3: TextureRect = $HealthContainer/Heart3
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
