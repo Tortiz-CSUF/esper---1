630a052c94ccb99e22b3c04657973977955064dc
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-30 03:39:48 +0000

    Haunted_Adventure_2D_19

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -4,6 +4,15 @@ extends CanvasLayer
 @onready var heart2: TextureRect = $HealthContainer/Heart2
 @onready var heart3: TextureRect = $HealthContainer/Heart3
 
+var full_heart: Texture2D = preload("res://Assets/hud/heart.png")
+var empty_heart: Texture2D = preload("res://Assets/hud/empty_heart.png")
+
+## Pause Menu Refs
+@onready var pause_overlay: ColorRect = $PauseOverlay
+@onready var resume_button: Button = $PauseOverlay/PauseMenu/ResumeButton
+@onready var pause_settings_button: Button = $PauseOverlay/PauseMenu/PauseSettingsButton
+@onready var main_menu_button: Button = $PauseOverlay/PauseMenu/MainMenuButton
+
 
 
 # Called when the node enters the scene tree for the first time.
