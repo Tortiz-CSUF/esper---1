e3c68a317b9babdd0ce0eb38fedab2c35e36e33c
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-30 02:39:26 +0000

    Haunted_Adventure_2D_8

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,3 +12,30 @@ var health: int = 3
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var footstep_sfx: AudioStreamPlayer2D = $FootstepSFX
 @onready var shoot_cooldown: Timer = $ShootCooldown
+
+# Bullet Scene - To be implemented
+var bullet_scene: PackedScene =  null
+
+# Direction Tracking 
+var last_direction: Vector2 = Vector2.DOWN
+
+func _ready() -> void:
+	if ResourceLoader.exists("res://Scenes/bullet.tscn"):		## MAKE SURE SCENE PATH MATCHES 
+		bullet_scene = load("res://Scenes/bullet.tscn")
+	animated_sprite.play("idle")
+	
+
+func _physics_process(delta: float) -> void:
+	var input_dir: Vector2 = Vector2.ZERO
+	input_dir.x = Input.get_axis("move_left", "move_right")
+	input_dir.y = Input.get_axis("move_up", "move_down")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
