1eeffa882a2763483a9f5600501a28d3390bcb47
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-31 22:37:45 +0000

    Haunted_Adventure_2D_9

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -20,9 +20,10 @@ var current_room_instance: Node2D = null
 ## RNG for Doors
 var starting_door_count: int = 1
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	total_rooms = GameManager.get_room_count()
+	go_to_room(0)
 
 
 func go_to_room(room_index: int) -> void:
--- Scripts/introduction.gd
+++ Scripts/introduction.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
-		get_tree().change_scene_to_file("[PLACEHOLDER FOR GAME SCENE]")
+		get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
